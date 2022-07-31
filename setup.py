cf644f76349a7ca8e561cc160858ac14cd012a96
lslattery11 <lslatte2@illinois.edu>
2022-07-31 10:36:54 -0500

    update to setup/readme/.gitigmore

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_namespace_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="wafer-defect-detection",
+    version=1.0,
+    python_requires=">= 3.9",
+    packages=find_namespace_packages(where="src", exclude=("test",)),
+    package_dir={"": "src"},
+    install_requires=[
+        setuptools,
+        numpy,
+        pytorch,
+        torchvision,
+        pandas,
+        matplotlib,
+        ipykernel,
+        wandb,
+        scikit-learn,
+        imgaug,
+        pylint,
+        tox,
+        coverage,
+        tqdm,
+        flake8,
+    ],
+    include_package_data=True,
+    url="https://github.com/lslattery11/wafer-defect-detection/",
+    description=(
+        "A package for defect detection in silicon wafers."
+    ),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
