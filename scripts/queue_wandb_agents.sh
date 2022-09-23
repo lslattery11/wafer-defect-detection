1e573dfa06b36a4d9b73d1cc1f084d63aa4d7b66
lslattery11 <lslatte2@illinois.edu>
2022-09-23 18:45:12 -0500

    update to hyper paremter sweeps

--- scripts/queue_wandb_agents.sh
+++ scripts/queue_wandb_agents.sh
@@ -3,5 +3,5 @@
 parallel \
     --jobs 10 \
     """
-    cd ../ ; wandb agent --count 1 lslattery/wafer-defect-detection/w6gxwl7y
+    cd ../ ; wandb agent --count 10 lslattery/wafer-defect-detection/w6gxwl7y
     """ ::: $(seq 0 9)
