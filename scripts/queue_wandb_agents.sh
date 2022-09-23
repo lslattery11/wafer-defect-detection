a3f60e440952dd1ac0b05417fef6b579b4f35734
lslattery11 <lslatte2@illinois.edu>
2022-09-22 22:13:21 -0500

    update to queue

--- scripts/queue_wandb_agents.sh
+++ scripts/queue_wandb_agents.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 parallel \
-    --jobs 20 \
+    --jobs 10 \
     """
     cd ../ ; wandb agent --count 1 lslattery/wafer-defect-detection/vyss3un0
-    """ ::: $(seq 0 19)
+    """ ::: $(seq 0 9)
