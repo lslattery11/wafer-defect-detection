695815f7d6dca9d56486a838c3e2242cfd038e17
lslattery11 <lslatte2@illinois.edu>
2022-09-23 18:51:24 -0500

    update to queue

--- scripts/queue_wandb_agents.sh
+++ scripts/queue_wandb_agents.sh
@@ -3,5 +3,5 @@
 parallel \
     --jobs 10 \
     """
-    cd ../ ; wandb agent --count 10 lslattery/wafer-defect-detection/m89ah3n1
+    cd ../ ; wandb agent --count 10 lslattery/wafer-defect-detection/g9xnzgke
     """ ::: $(seq 0 9)
