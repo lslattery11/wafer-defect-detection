84513980faffd16be59428d402c8a003dc4f126b
lslattery11 <lslatte2@illinois.edu>
2022-09-30 10:42:42 -0500

    update to run_model.py

--- run_model.py
+++ run_model.py
@@ -67,6 +67,8 @@ if __name__ == '__main__':
     cnn_channels=tuple(2**(i) for i in range(args.num_cnn_layers))
     spp_output_sizes=[(1+2*i,1+2*i) for i in range(args.num_spp_outputs)]
 
+    print(args.binary==True)
+    print(args.just_defects==True)
     if args.binary==True:
         linear_output_sizes=tuple(2*2**(i-1) for i in range(args.num_linear_layers,0,-1))
     elif args.just_defects==True:
